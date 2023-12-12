ea81cf3f1bd394c2ae473f7742645073d0bcc1ec
rdharun <dharunr@testvagrant.com>
2023-12-12 11:39:40 +0530

    added jsonBody

--- src/test/java/UserLoginTest.java
+++ src/test/java/UserLoginTest.java
@@ -1,6 +1,9 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+import java.io.File;
+
 import static org.testng.Assert.assertEquals;
 import static org.testng.AssertJUnit.assertNotNull;
 
@@ -10,13 +13,11 @@ public class UserLoginTest extends BaseTest {
     @Test
     public void validateUserLogin() {
 
+        File jsonBody = new File("src/main/resources/userLogin.json");
+
         Response response = RestAssured.given()
                 .header("Content-Type", "application/json")
-                .body("\n" +
-                        "{\n" +
-                        "   \"email\":\"" + "abc@gmail.com" + "\",\n" +
-                        "   \"password\":\"" + "123456" + "\"\n" +
-                        "}\n")
+                .body(jsonBody)
                 .when()
                 .post("/api/auth/login");
 
