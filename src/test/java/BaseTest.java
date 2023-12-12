d2f8c2581b573d45c479552a9318d07a5ebb6fd9
rdharun <dharunr@testvagrant.com>
2023-12-12 15:02:17 +0530

    Modified base test

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,12 +1,28 @@
 import io.restassured.RestAssured;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import utilities.PropertyUtils;
+import org.testng.Assert;
+
 
 public class BaseTest {
 
-    @BeforeClass
+    @BeforeTest
     public void setUp() {
         String baseUrl = PropertyUtils.getProperty("base_url");
         RestAssured.baseURI = baseUrl;
     }
+
+    @AfterTest
+    public void tearDown() {
+
+    }
+
+
+    // Utility method for asserting HTTP status code
+    protected void assertStatusCode(int actualStatusCode, int expectedStatusCode) {
+        Assert.assertEquals(actualStatusCode, expectedStatusCode, "Unexpected status code");
+    }
+
+
 }
