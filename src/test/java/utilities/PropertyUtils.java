300cd294bf01e9c3a0f63d333da756861a26f37a
rdharun <dharunr@testvagrant.com>
2023-12-18 19:44:41 +0530

    modified properity file

--- src/test/java/utilities/PropertyUtils.java
+++ src/test/java/utilities/PropertyUtils.java
@@ -1,10 +1,16 @@
 package utilities;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class PropertyUtils {
+
+    private static final Logger logger = LogManager.getLogger(PropertyUtils.class);
+
 
     public static String getProperty(String propertyName, String environment) {
         String configPropertyFilePath = "src/main/resources/" + environment + ".properties";
@@ -12,10 +18,10 @@ public class PropertyUtils {
         try {
             FileInputStream configFile = new FileInputStream(configPropertyFilePath);
             properties.load(configFile);
-            return properties.getProperty(propertyName);
         } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+            logger.error("Failed to load environment-specific properties file", e);
+            throw new RuntimeException("Configuration could not be loaded for environment: " + environment, e);
         }
+        return properties.getProperty(propertyName);
     }
 }
